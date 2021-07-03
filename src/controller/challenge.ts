87644883362b922012ec43d4bf953a45d1671476
hyundang <hyunjin5697@yonsei.ac.kr>
2021-07-03 16:45:16 +0900

    feat: challenge api get, post, patch

--- src/controller/challenge.ts
+++ src/controller/challenge.ts
@@ -2,19 +2,58 @@ import { Router, Request, Response } from "express";
 import { verify } from "src/library/jwt";
 import { returnCode } from "src/library/returnCode";
 import { response, dataResponse } from "src/library/response";
-import { postChallenge, patchChallenge } from "src/service/challengeService";
+import {
+  getChallengeAll,
+  postChallenge,
+  patchChallenge,
+  postChallengeLike,
+} from "src/service/challengeService";
 import { IChallengePostDTO } from "src/interfaces/IChallenge";
 
 const router = Router();
 
 /**
+ *  @챌린지_회고_전체_가져오기
+ *  @route Get challenge
+ *  @access Private
+ */
+
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    // 토큰 검사
+    if (req.headers.authorization == null) {
+      response(res, returnCode.BAD_REQUEST, "토큰 값이 요청되지 않았습니다");
+    }
+
+    //토큰
+    const token = req.headers.authorization;
+    const decoded = verify(token);
+
+    // 토큰 확인
+    if (decoded === -3) {
+      response(res, returnCode.UNAUTHORIZED, "만료된 토큰입니다");
+    } else if (decoded === -2) {
+      response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
+    }
+    const data = await getChallengeAll();
+
+    // 회고 전체 불러오기 성공
+    const challengeAll = data;
+    dataResponse(res, returnCode.OK, "회고 전체 불러오기 성공", challengeAll);
+  } catch (err) {
+    console.error(err.message);
+    response(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
+  }
+});
+
+/**
  *  @챌린지_회고_등록
- *  @route Post challenge/:id
- *  @access Public
+ *  @route Post challenge/:userId
+ *  @access Private
  */
 
 router.post<unknown, unknown, IChallengePostDTO>(
-  "/:id",
+  "/",
   async (req: Request, res: Response) => {
     try {
       // 토큰 검사
@@ -25,7 +64,6 @@ router.post<unknown, unknown, IChallengePostDTO>(
       //토큰
       const token = req.headers.authorization;
       const decoded = verify(token);
-      const userId = decoded.userIdx;
 
       // 토큰 확인
       if (decoded === -3) {
@@ -33,7 +71,9 @@ router.post<unknown, unknown, IChallengePostDTO>(
       } else if (decoded === -2) {
         response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
       }
-      const data = await postChallenge(req.params.id, req.body);
+
+      const userID = decoded.user.id;
+      const data = await postChallenge(userID, req.body);
 
       // 요청 바디가 부족할 경우
       if (data === -1) {
@@ -55,6 +95,12 @@ router.post<unknown, unknown, IChallengePostDTO>(
   }
 );
 
+/**
+ *  @챌린지_회고_수정
+ *  @route Patch challenge/:challengeId
+ *  @access Private
+ */
+
 router.patch("/:id", async (req: Request, res: Response) => {
   try {
     // 토큰 검사
@@ -65,7 +111,6 @@ router.patch("/:id", async (req: Request, res: Response) => {
     //토큰
     const token = req.headers.authorization;
     const decoded = verify(token);
-    const userId = decoded.userIdx;
 
     // 토큰 확인
     if (decoded === -3) {
@@ -81,6 +126,46 @@ router.patch("/:id", async (req: Request, res: Response) => {
     } else {
       const challenge = data;
       dataResponse(res, returnCode.OK, "회고 수정 성공", challenge);
+    }
+  } catch (err) {
+    console.error(err.message);
+    response(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
+  }
+});
+
+/**
+ *  @챌린지_회고_좋아요_등록
+ *  @route Post challenge/like
+ *  @access Private
+ */
+
+router.post("/like/:id", async (req: Request, res: Response) => {
+  try {
+    // 토큰 검사
+    if (req.headers.authorization == null) {
+      response(res, returnCode.BAD_REQUEST, "토큰 값이 요청되지 않았습니다");
+    }
+
+    //토큰
+    const token = req.headers.authorization;
+    const decoded = verify(token);
+
+    // 토큰 확인
+    if (decoded === -3) {
+      response(res, returnCode.UNAUTHORIZED, "만료된 토큰입니다");
+    } else if (decoded === -2) {
+      response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
+    }
+
+    const userID = decoded.user.id;
+    console.log(req.params.id);
+    const data = await postChallengeLike(req.params.id, userID);
+
+    if (data === -1) {
+      response(res, returnCode.NOT_FOUND, "요청 경로가 올바르지 않습니다");
+    } else {
+      const challenge = data;
+      dataResponse(res, returnCode.OK, "좋아요 등록 성공", challenge);
     }
   } catch (err) {
     console.error(err.message);
--- src/service/challengeService.ts
+++ src/service/challengeService.ts
@@ -1,19 +1,31 @@
-//
+// models
 import Challenge from "src/models/Challenge";
 import User from "src/models/User";
 // DTO
 import { IChallengePostDTO } from "src/interfaces/IChallenge";
 
 /**
+ *  @챌린지_회고_전체_가져오기
+ *  @route Get /challenge
+ */
+
+export const getChallengeAll = async () => {
+  const data = await Challenge.find();
+  // 코멘트 추가하기
+
+  return data;
+};
+
+/**
  *  @챌린지_회고_등록
- *  @route Post api/challenge/:id
+ *  @route Post api/challenge/:userId
  *  @body author, good, bad, learn, interest, generation
  *  @error
  *      1. 요청 바디 부족
  *      2. 유저 id 잘못됨
  */
 
-export const postChallenge = async (userId, body) => {
+export const postChallenge = async (userID, body) => {
   const { good, bad, learn, interest, generation } = body;
 
   // 1. 요청 바디 부족
@@ -22,13 +34,13 @@ export const postChallenge = async (userId, body) => {
   }
 
   // 2. 유저 id 잘못됨
-  let user = await User.findById(userId);
+  let user = await User.findById(userID);
   if (!user) {
     return -2;
   }
 
   const challenge = new Challenge({
-    user: userId,
+    user: userID,
     good,
     bad,
     learn,
@@ -38,34 +50,84 @@ export const postChallenge = async (userId, body) => {
 
   await challenge.save();
 
-  let data = Challenge.findOne({ user: userId }).populate("user", ["nickname"]);
+  let data = Challenge.findOne({ user: userID }).populate("user", ["nickname"]);
 
   return data;
 };
 
 /**
  *  @챌린지_회고_수정
- *  @route Post api/challenge/:id
+ *  @route PATCH api/challenge/:challengeId
  *  @body good, bad, learn
  *  @error
- *      1. 요청 바디 부족
+ *      1. 회고록 id 잘못됨
  */
 
-export const patchChallenge = async (challengeId, body) => {
+export const patchChallenge = async (challengeID, body) => {
   const { good, bad, learn } = body;
 
   // 1. 회고록 id 잘못됨
-  const challenge = await Challenge.findById(challengeId);
+  const challenge = await Challenge.findById(challengeID);
   if (!challenge) {
     return -1;
   }
 
+  const updateDate = new Date();
+
   await Challenge.update(
-    { _id: challengeId },
+    { _id: challengeID },
     {
       good,
       bad,
       learn,
+      updatedAt: updateDate,
     }
   );
+};
+
+/**
+ *  @챌린지_회고_삭제
+ *  @route DELETE api/challenge/:challengeId
+ *  @error
+ *      1. 회고록 id 잘못됨
+ */
+
+//  export const deleteChallenge = async (challengeID) => {
+
+//   // 1. 회고록 id 잘못됨
+//   const challenge = await Challenge.findById(challengeID);
+//   if (!challenge) {
+//     return -1;
+//   }
+
+// };
+
+/**
+ *  @챌린지_회고_좋아요_등록
+ *  @route POST /challenge/like
+ *  @error
+ *      1. 회고록 id 잘못됨
+ */
+
+export const postChallengeLike = async (challengeID, userID) => {
+  // 1. 회고록 id 잘못됨
+  const challenge = await Challenge.findById(challengeID);
+
+  if (!challenge) {
+    return -1;
+  }
+
+  // 챌린지 글의 like 1 증가
+  await Challenge.findOneAndUpdate(
+    { _id: challengeID },
+    {
+      $inc: { likes: 1 },
+    }
+  );
+  // 유저 likes 필드에 챌린지 id 추가
+  const user = await User.findById(userID);
+  user.likes.chanllengeLikes.push(challengeID);
+  await user.save();
+
+  return { _id: challengeID };
 };
