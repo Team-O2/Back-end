1009845b5e8e7d60bdf7125b8026efe6533b047d
Go Seongyong <holmir97>
2021-07-05 15:37:07 +0900

    fix: add admin route

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/auth", require("src/controller/auth"));
 app.use("/challenge", require("src/controller/challenge"));
 app.use("/admin", require("src/controller/admin"));
-// app.use("/concert", require("src/controller/concert"))
+app.use("/concert", require("src/controller/concert"));
 
 // error handler
 app.use(function (err, req, res, next) {
