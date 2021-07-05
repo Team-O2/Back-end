c46200e6b40ec90fbfbccfdb946936fafa66ca98
Go Seongyong <holmir97>
2021-07-05 15:39:01 +0900

    feat: concert 기능 추가

--- src/controller/concert.ts
+++ src/controller/concert.ts
@@ -1,17 +1,29 @@
-import express, { Request, Response } from "express";
-import { verify } from "library/jwt";
-import { returnCode } from "library/returnCode";
-import { response, dataResponse, dataTokenResponse } from "library/response";
-import { concertService } from "service/concertService";
+import { Router, Request, Response } from "express";
+import { verify } from "src/library/jwt";
+import { returnCode } from "src/library/returnCode";
+import { response, dataResponse } from "src/library/response";
+// service
+import {
+  postConcertComment,
+  postConcertLike,
+  deleteConcertLike,
+  getConcertAll,
+  getConcertSearch,
+  postConcertScrap,
+  deleteConcertScrap,
+} from "src/service/concertService";
+// DTO
+import { IConcertPostDTO } from "src/interfaces/IConcert";
 
-const router = express.Router();
+const router = Router();
 
 /**
- *  @오투콘서트 (share together) 전체 조회
- *  @route Get concert
- *  @access Public
+ *  @오투콘서트_전체_가져오기
+ *  @route Get /concert
+ *  @access Private
  */
-router.post("/", async (req: Request, res: Response) => {
+
+router.get("/", async (req: Request, res: Response) => {
   try {
     // 토큰 검사
     if (req.headers.authorization == null) {
@@ -21,16 +33,280 @@ router.post("/", async (req: Request, res: Response) => {
     //토큰
     const token = req.headers.authorization;
     const decoded = verify(token);
-    const userId = decoded.userIdx;
 
     // 토큰 확인
-    if (decoded == -3) {
+    if (decoded === -3) {
       response(res, returnCode.UNAUTHORIZED, "만료된 토큰입니다");
-    } else if (decoded == -2) {
+    }
+    if (decoded === -2) {
+      response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
+    }
+    const data = await getConcertAll();
+
+    // 회고 전체 불러오기 성공
+    const concert = data;
+    dataResponse(res, returnCode.OK, "콘서트 전체 불러오기 성공", concert);
+  } catch (err) {
+    console.error(err.message);
+    response(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
+  }
+});
+
+/**
+ *  @오투콘서트_검색_또는_필터
+ *  @route Get /concert/search?tag=관심분야&ismine=내글만보기여부&keyword=검색할단어
+ *  @access Private
+ */
+
+router.get("/search", async (req: Request, res: Response) => {
+  try {
+    // 토큰 검사
+    if (req.headers.authorization == null) {
+      response(res, returnCode.BAD_REQUEST, "토큰 값이 요청되지 않았습니다");
+    }
+
+    //토큰
+    const token = req.headers.authorization;
+    const decoded = verify(token);
+
+    // 토큰 확인
+    if (decoded === -3) {
+      response(res, returnCode.UNAUTHORIZED, "만료된 토큰입니다");
+    }
+    if (decoded === -2) {
       response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
     }
 
-    const data = await concertService.getConcertList(req.body);
+    const data = await getConcertSearch(
+      req.query.tag,
+      req.query.isMine,
+      req.query.keyword,
+      decoded.user.id
+    );
+
+    // 검색 불러오기 성공
+    const concertSearch = data;
+    dataResponse(res, returnCode.OK, "검색 성공", concertSearch);
+  } catch (err) {
+    console.error(err.message);
+    response(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
+  }
+});
+
+/**
+ *  @콘서트_댓글_등록
+ *  @route Post /concert/comment/:concertID
+ *  @access Private
+ */
+
+router.post("/comment/:id", async (req: Request, res: Response) => {
+  try {
+    // 토큰 검사
+    if (req.headers.authorization == null) {
+      response(res, returnCode.BAD_REQUEST, "토큰 값이 요청되지 않았습니다");
+    }
+
+    //토큰
+    const token = req.headers.authorization;
+    const decoded = verify(token);
+
+    // 토큰 확인
+    if (decoded === -3) {
+      response(res, returnCode.UNAUTHORIZED, "만료된 토큰입니다");
+    }
+    if (decoded === -2) {
+      response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
+    }
+
+    const userID = decoded.user.id;
+    const data = await postConcertComment(req.params.id, userID, req.body);
+
+    // 회고 id가 잘못된 경우
+    if (data === -1) {
+      response(res, returnCode.NOT_FOUND, "요청 경로가 올바르지 않습니다");
+    }
+    //  요청 바디가 부족한 경우
+    if (data === -2) {
+      response(res, returnCode.BAD_REQUEST, "요청 값이 올바르지 않습니다");
+    }
+    // 부모 댓글 id가 잘못된 경우
+    if (data === -3) {
+      response(res, returnCode.BAD_REQUEST, "부모 댓글 id가 올바르지 않습니다");
+    }
+    // 댓글 등록 성공
+    const concertComment = data;
+    dataResponse(res, returnCode.OK, "댓글 등록 성공", concertComment);
+  } catch (err) {
+    console.error(err.message);
+    response(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
+  }
+});
+
+/**
+ *  @오투콘서트_좋아요_등록
+ *  @route Post /concert/like/:concertID
+ *  @access Private
+ */
+
+router.post("/like/:id", async (req: Request, res: Response) => {
+  try {
+    // 토큰 검사
+    if (req.headers.authorization == null) {
+      response(res, returnCode.BAD_REQUEST, "토큰 값이 요청되지 않았습니다");
+    }
+
+    //토큰
+    const token = req.headers.authorization;
+    const decoded = verify(token);
+
+    // 토큰 확인
+    if (decoded === -3) {
+      response(res, returnCode.UNAUTHORIZED, "만료된 토큰입니다");
+    }
+    if (decoded === -2) {
+      response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
+    }
+
+    const userID = decoded.user.id;
+    const data = await postConcertLike(req.params.id, userID);
+
+    // 회고 id가 잘못된 경우
+    if (data === -1) {
+      response(res, returnCode.NOT_FOUND, "요청 경로가 올바르지 않습니다");
+    }
+    // 이미 좋아요 한 글일 경우
+    if (data === -2) {
+      response(res, returnCode.BAD_REQUEST, "이미 좋아요 한 글입니다");
+    }
+    // 좋아요 등록 성공
+    const concert = data;
+    dataResponse(res, returnCode.OK, "좋아요 등록 성공", concert);
+  } catch (err) {
+    console.error(err.message);
+    response(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
+  }
+});
+
+/**
+ *  @오투콘서트_좋아요_삭제하기
+ *  @route Delete /concert/like/:concertID
+ *  @access Private
+ */
+
+router.delete("/like/:id", async (req: Request, res: Response) => {
+  try {
+    // 토큰 검사
+    if (req.headers.authorization == null) {
+      response(res, returnCode.BAD_REQUEST, "토큰 값이 요청되지 않았습니다");
+    }
+
+    //토큰
+    const token = req.headers.authorization;
+    const decoded = verify(token);
+
+    // 토큰 확인
+    if (decoded === -3) {
+      response(res, returnCode.UNAUTHORIZED, "만료된 토큰입니다");
+    }
+    if (decoded === -2) {
+      response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
+    }
+
+    const userID = decoded.user.id;
+    const data = await deleteConcertLike(req.params.id, userID);
+
+    // 콘서트 id가 잘못된 경우
+    if (data === -1) {
+      response(res, returnCode.NOT_FOUND, "요청 경로가 올바르지 않습니다");
+    } // 좋아요 한 개수가 0인 경우
+    if (data === -2) {
+      response(res, returnCode.BAD_REQUEST, "좋아요 개수가 0");
+    }
+    // 좋아요 삭제 성공
+    const concert = data;
+    dataResponse(res, returnCode.OK, "좋아요 삭제 성공", concert);
+  } catch (err) {
+    console.error(err.message);
+    response(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
+  }
+});
+/**
+ *  @유저_챌린지_회고_스크랩하기
+ *  @route Post /concert/scrap/:concertID
+ *  @access Private
+ */
+router.post("/scrap/:id", async (req: Request, res: Response) => {
+  try {
+    // 토큰 검사
+    if (req.headers.authorization == null) {
+      response(res, returnCode.BAD_REQUEST, "토큰 값이 요청되지 않았습니다");
+    }
+
+    //토큰
+    const token = req.headers.authorization;
+    const decoded = verify(token);
+
+    // 토큰 확인
+    if (decoded === -3) {
+      response(res, returnCode.UNAUTHORIZED, "만료된 토큰입니다");
+    }
+    if (decoded === -2) {
+      response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
+    }
+    const userID = decoded.user.id;
+    const data = await postConcertScrap(req.params.id, userID);
+
+    // 회고 id가 잘못된 경우
+    if (data === -1) {
+      response(res, returnCode.NOT_FOUND, "요청 경로가 올바르지 않습니다");
+    }
+    // 이미 유저가 스크랩한 글일 경우
+    if (data === -2) {
+      response(res, returnCode.BAD_REQUEST, "이미 스크랩 된 글입니다");
+    }
+    // 회고 스크랩 성공
+    dataResponse(res, returnCode.OK, "콘서트 스크랩 성공", data);
+  } catch (err) {
+    console.error(err.message);
+    response(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
+  }
+});
+
+/**
+ *  @오투콘서트_회고_스크랩_취소하기
+ *  @route Delete /concert/scrap/:concertID
+ *  @access Private
+ */
+router.delete("/scrap/:id", async (req: Request, res: Response) => {
+  try {
+    // 토큰 검사
+    if (req.headers.authorization == null) {
+      response(res, returnCode.BAD_REQUEST, "토큰 값이 요청되지 않았습니다");
+    }
+
+    //토큰
+    const token = req.headers.authorization;
+    const decoded = verify(token);
+
+    // 토큰 확인
+    if (decoded === -3) {
+      response(res, returnCode.UNAUTHORIZED, "만료된 토큰입니다");
+    }
+    if (decoded === -2) {
+      response(res, returnCode.UNAUTHORIZED, "적합하지 않은 토큰입니다");
+    }
+    const data = await deleteConcertScrap(req.params.id, decoded.user.id);
+
+    // 콘서트 id가 잘못된 경우
+    if (data === -1) {
+      response(res, returnCode.NOT_FOUND, "요청 경로가 올바르지 않습니다");
+    }
+    // 스크랩 하지 않은 글일 경우
+    if (data === -2) {
+      response(res, returnCode.BAD_REQUEST, "스크랩 하지 않은 글입니다");
+    }
+    // 스크랩 취소 성공
+    dataResponse(res, returnCode.OK, "콘서트 스크랩 취소 성공", data);
   } catch (err) {
     console.error(err.message);
     response(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
--- src/service/concertService.ts
+++ src/service/concertService.ts
@@ -1,19 +1,286 @@
-// import Concert from "../models/Concert";
+// models
+import Concert from "src/models/Concert";
+import User from "src/models/User";
+import Comment from "src/models/Comment";
+// DTO
+import { IConcertPostDTO } from "src/interfaces/IConcert";
 
-// /**
-//  *  @오투콘서트 (share together) 전체 조회
-//  *  @route Get concert
-//  *  @access Public
-//  *  @param token
-//  *  @error
-//  *      1. 요청 바디 부족
-//  *      2. 아이디가 존재하지 않음
-//  *      3. 패스워드가 올바르지 않음
-//  */
-// async function getConcertList(body) {
-//   const;
-// }
+/**
+ *  @오투콘서트_전체_가져오기
+ *  @route Get /concert
+ */
+export const getConcertAll = async () => {
+  // 댓글, 답글 populate
+  // isDelete = true 인 애들만 가져오기
+  const concerts = await Concert.find({ isDeleted: false })
+    .populate("user", ["nickname"])
+    .populate({
+      path: "comments",
+      select: ["userID, text"],
+      populate: [
+        {
+          path: "childrenComment",
+          select: ["userID", "text"],
+          populate: {
+            path: "userID",
+            select: ["nickname"],
+          },
+        },
+        {
+          path: "userID",
+          select: ["nickname"],
+        },
+      ],
+    });
 
-// module.exports = {
-//   getConcertList,
-// };
+  return concerts;
+};
+
+/**
+ *  @오투콘서트_검색_또는_필터
+ *  @route Get /concert/search?tag=관심분야&ismine=내글만보기여부&keyword=검색할단어
+ */
+export const getConcertSearch = async (tag, isMine, keyword, userID) => {
+  // 댓글, 답글 populate
+  // isDelete = true 인 애들만 가져오기
+  const concerts = await Concert.find({ isDeleted: false })
+    .populate("user", ["nickname"])
+    .populate({
+      path: "comments",
+      select: ["userID, text"],
+      populate: [
+        {
+          path: "childrenComment",
+          select: ["userID", "text"],
+          populate: {
+            path: "userID",
+            select: ["nickname"],
+          },
+        },
+        {
+          path: "userID",
+          select: ["nickname"],
+        },
+      ],
+    });
+
+  let filteredData = concerts;
+
+  // 관심분야 필터링
+  if (tag !== "") {
+    filteredData = filteredData.filter((fd) => {
+      if (fd.interest.includes(tag)) return fd;
+    });
+  }
+
+  // 내가 쓴 글 필터링
+  if (isMine === "1") {
+    filteredData = filteredData.filter((fd) => {
+      if (fd.user._id === userID) return fd;
+    });
+  }
+
+  // 검색 단어 필터링
+  if (keyword !== "") {
+    filteredData = filteredData.filter((fd) => {
+      if (
+        fd.text.includes(keyword) ||
+        fd.title.includes(keyword) ||
+        fd.hashtag.includes(keyword)
+      )
+        return fd;
+    });
+  }
+
+  return filteredData;
+};
+
+/**
+ *  @콘서트_댓글_등록
+ *  @route Post /concert/comment/:concertID
+ *  @access Private
+ *  @error
+ *      1. 회고록 id 잘못됨
+ *      2. 요청 바디 부족
+ *      3. 부모 댓글 id 값이 유효하지 않을 경우
+ */
+export const postConcertComment = async (concertID, userID, body) => {
+  const { parentID, text } = body;
+
+  // 1. 회고록 id 잘못됨
+  const concert = await Concert.findById(concertID);
+
+  if (!concert) {
+    return -1;
+  }
+  // 2. 요청 바디 부족
+  if (!text) {
+    return -2;
+  }
+
+  let comment;
+  // 답글인 경우
+  if (parentID) {
+    const parentComment = await Comment.findById(parentID);
+
+    // 3. 부모 댓글 id 값이 유효하지 않을 경우
+    if (!parentComment) {
+      return -3;
+    }
+
+    comment = new Comment({
+      postModel: "Concert",
+      post: concertID,
+      userID: userID,
+      parentComment: parentID,
+      text,
+    });
+    await comment.save();
+
+    await parentComment.childrenComment.push(comment._id);
+    await parentComment.save();
+  } else {
+    // 댓글인 경우
+    comment = new Comment({
+      postModel: "Concert",
+      post: concertID,
+      userID: userID,
+      text,
+    });
+
+    await comment.save();
+  }
+
+  const user = await User.findById(userID);
+
+  return {
+    _id: comment._id,
+    nickname: user.nickname,
+    text: text,
+    createdAt: comment.createdAt,
+  };
+};
+
+/**
+ *  @오투콘서트_좋아요_등록
+ *  @route Post /concert/like/:concertID
+ *  @error
+ *      1. 콘서트 id 잘못됨
+ *      2. 이미 좋아요 한 글일 경우
+ */
+export const postConcertLike = async (concertID, userID) => {
+  // 1. 콘서트 id 잘못됨
+  const concert = await Concert.findById(concertID);
+
+  if (!concert) {
+    return -1;
+  }
+
+  const user = await User.findById(userID);
+  // 2. 이미 좋아요 한 글일 경우
+  if (user.likes.concertLikes.includes(concertID)) {
+    return -2;
+  }
+
+  // 챌린지 글의 like 1 증가
+  await Concert.findOneAndUpdate(
+    { _id: concertID },
+    {
+      $inc: { likes: 1 },
+    }
+  );
+  // 유저 likes 필드에 챌린지 id 추가
+  user.likes.concertLikes.push(concertID);
+  await user.save();
+
+  return { _id: concertID };
+};
+
+/**
+ *  @오투콘서트_좋아요_삭제
+ *  @route Delete /concert/like/:concertID
+ *  @error
+ *      1. 회고록 id 잘못됨
+ *      2. 좋아요 개수가 0
+ */
+export const deleteConcertLike = async (concertID, userID) => {
+  const concert = await Concert.findById(concertID);
+
+  // 1. 회고록 id 잘못됨
+  if (!concert) {
+    return -1;
+  }
+
+  // 2. 좋아요 개수가 0
+  if (concert.likes === 0) {
+    return -2;
+  }
+
+  // 챌린지 글의 like 1 감소
+  await Concert.findOneAndUpdate(
+    { _id: concertID },
+    {
+      $inc: { likes: -1 },
+    }
+  );
+  // 유저 likes 필드에 챌린지 id 삭제
+  const user = await User.findById(userID);
+  user.likes.concertLikes.pull(concertID);
+  await user.save();
+
+  return { _id: concertID };
+};
+
+/**
+ *  @오투콘서트_회고_스크랩하기
+ *  @route Post /user/concert/:concertID
+ *  @error
+ *      1. 콘서트 id 잘못됨
+ *      2. 이미 스크랩 한 회고일 경우
+ */
+export const postConcertScrap = async (concertID, userID) => {
+  // 1. 회고 id 잘못됨
+  let concert = await Concert.findById(concertID);
+  if (!concert) {
+    return -1;
+  }
+
+  const user = await User.findById(userID);
+
+  // 2. 이미 스크랩 한 회고인 경우
+  if (user.scraps.concertScraps.includes(concertID)) {
+    return -2;
+  }
+
+  user.scraps.concertScraps.push(concertID);
+  await user.save();
+
+  return { _id: concertID };
+};
+
+/**
+ *  @유저_챌린지_회고_스크랩_취소하기
+ *  @route Delete /user/concert/:concertID
+ *  @error
+ *      1. 콘서트 id 잘못됨
+ *      2. 스크랩 하지 않은 글일 경우
+ */
+export const deleteConcertScrap = async (concertID, userID) => {
+  // 1. 회고 id 잘못됨
+  let concert = await Concert.findById(concertID);
+  if (!concert) {
+    return -1;
+  }
+
+  const user = await User.findById(userID);
+  // 2. 스크랩하지 않은 글일 경우
+  if (!user.scraps.concertScraps.includes(concertID)) {
+    return -2;
+  }
+
+  // 유저 likes 필드에 챌린지 id 삭제
+  user.scraps.concertScraps.pull(concertID);
+  await user.save();
+
+  return { _id: concertID };
+};
