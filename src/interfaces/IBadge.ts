ce190b797c9d060b6f6ab65daabfc28bf8232587
Go Seongyong <holmir97>
2021-07-06 22:07:29 +0900

    feat: Badge 인터페이스 추가

---
+++ src/interfaces/IBadge.ts
@@ -0,0 +1,15 @@
+export interface IAdmin {
+  welcomeBadge: Boolean;
+  firstJoinBadge: Boolean;
+  firstWriteBadge: Boolean;
+  oneCommentBadge: Boolean;
+  fiveCommentBadge: Boolean;
+  oneLikeBadge: Boolean;
+  fiveLikeBadge: Boolean;
+  loginBadge: Boolean;
+  marketingBadge: Boolean;
+  runMySelfBadge: Boolean;
+  firstReplyBadge: Boolean;
+  concertScrapBadge: Boolean;
+  challengeBadge: Number;
+}
