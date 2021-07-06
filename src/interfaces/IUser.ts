341ed3d98b9ed390addb3e1ad27e792b4bb98d9b
Go Seongyong <holmir97>
2021-07-06 22:08:02 +0900

    fix: Badge 인터페이스 속성 추가

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { IScraps } from "./IScraps";
 import { ILikes } from "./ILikes";
+import { IBadge } from "./IBadge";
 
 export interface IUser {
   _id: mongoose.Schema.Types.ObjectId;
@@ -11,26 +12,10 @@ export interface IUser {
   marpolicy: Boolean;
   gender: Number;
   challengeCNT: Number;
-  challengebadgeCNT: Number;
+  Badge: IBadge;
   badgeCNT: Number;
   likes: ILikes;
   scraps: IScraps;
   userType: Number;
   img: string;
-}
-
-export interface IUserDTO {
-  _id: mongoose.Schema.Types.ObjectId;
-  email: string;
-  password: string;
-  nickname: string;
-  interest: [string];
-  marpolicy: Boolean;
-  gender: Number;
-  challengeCNT: Number;
-  challengebadgeCNT: Number;
-  badgeCNT: Number;
-  likes: ILikes;
-  scraps: IScraps;
-  userType: Number;
 }
