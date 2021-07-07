9422bf02ac8c474125ecafeea37b2a8655c9e89b
Go Seongyong <holmir97>
2021-07-08 05:25:39 +0900

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,6 @@ app.use(express.json());
 // allow cors
 var cors = require("cors")();
 app.use(cors);
-app.get("/", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-});
 
 // route
 app.use("/auth", require("src/controller/auth"));
