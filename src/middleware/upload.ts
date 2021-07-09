9abf9098bb62cc2708bd64f1ddb36183e97a3701
Go Seongyong <holmir97>
2021-07-09 21:22:25 +0900

    feat: multer 함수 추가

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,20 +1,39 @@
-import aws from 'aws-sdk';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import config from 'src/config';
+import aws from "aws-sdk";
+import multer from "multer";
+import multerS3 from "multer-s3";
+import config from "src/config";
 
 const s3 = new aws.S3({
-    accessKeyId: config.awsS3AccessKey,
-    secretAccessKey: config.awsS3SecretAccessKey
+  accessKeyId: config.awsS3AccessKey,
+  secretAccessKey: config.awsS3SecretAccessKey,
 });
 
-export const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: config.awsBucket,
-        acl: 'public-read',
-        key: function(req, file, cb){
-            cb(null, 'images/origin/'+Date.now()+'.'+file.originalname.split('.').pop());
-        }
-    })
+export const uploadImg = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: config.awsBucket,
+    acl: "public-read",
+    Bucket: "o2-server/img",
+    key: function (req, file, cb) {
+      cb(
+        null,
+        "images/origin/" + Date.now() + "." + file.originalname.split(".").pop()
+      );
+    },
+  }),
+});
+
+export const uploadVideo = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: config.awsBucket,
+    acl: "public-read",
+    Bucket: "o2-server/video",
+    key: function (req, file, cb) {
+      cb(
+        null,
+        "images/origin/" + Date.now() + "." + file.originalname.split(".").pop()
+      );
+    },
+  }),
 });
